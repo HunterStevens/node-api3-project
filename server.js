e875a369df15957aae8743cfe21f1bb29292e1d5
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-06 14:16:38 -0600

    filled in Logger MiddleWare (MW) for requests

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const postDb = require('./posts/postDb');
+const userDb = require('./users/userDb');
 
 const server = express();
 
@@ -6,8 +8,27 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const today = new Date().toISOString();
+  today.slice(0,14);
+  console.log(`${req.method} to ${req.url} [${today}]`);
+}
+
+function validateUserId(req,res,next){
+
+}
+
+function validateUser(req,res,next){
+
+}
+
+function validatePost(req,res,next){
+
+}
+
 
 module.exports = server;
