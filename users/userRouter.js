85700769c08ae396d1203b979db739e79d9eb0e5
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-06 16:29:52 -0600

    Put Request working

--- users/userRouter.js
+++ users/userRouter.js
@@ -17,7 +17,16 @@ router.post('/', validateUser, (req, res) => {
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const newArticle = req.body;
+  newArticle.user_id = req.user.id;
 
+  postDb.insert(req.body)
+  .then(article =>{
+    res.status(200).json(article);
+  })
+  .catch(err =>{
+    res.status(500).json(err);
+  })
 });
 
 router.get('/', (req, res) => {
@@ -56,12 +65,39 @@ router.get('/:id/posts', validateUserId, (req, res) => {
 
 router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
-
+  userDb.remove(req.user.id)
+  .then(person =>{
+        userDb.get()
+      .then(people =>{
+        console.log('success on GET api/users request');
+        res.status(200).json(people)
+      })
+      .catch(err =>{
+        console.log(err);
+        res.status(500).json("userDB get error: ", err);
+      })
+  })
+  .catch(err =>{
+    res.status(500).json("userDB remove method error: ", err);
+  })
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
-  
+  userDb.update(req.user.id, req.body)
+  .then(notNeeded =>{
+    console.log(notNeeded);
+    userDb.getById(req.user.id)
+    .then(person=>{
+      res.status(200).json(person);
+    })
+    .catch(err =>{
+      res.status(500).json("Getting updated user Error: ",err);
+    })
+  })
+  .catch(err =>{
+    res.status(500).json("userDB update Error: ",err);
+  })
 });
 
 //custom middleware
@@ -94,11 +130,19 @@ function validateUser(req, res, next) {
   else{
     next();
   }
-
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(req.body.text === undefined || req.body === null){
+    res.status(400).json({ message: "missing user data" });
+  }
+  else if(req.body.text === "" || req.body.text === null){
+    res.status(404).json({ message: "missing required name field" });
+  }
+  else{
+    next();
+  }
 }
 
 module.exports = router;
