1b6fc213e7a1d7c2876c64ceda8bf335cedf1e98
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-06 15:41:57 -0600

    finished the GET requests in UserRouter

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const postRouter = require('./posts/postRouter');
 
 const server = express();
 
+server.use(express.json());
 server.use(logger);
-
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-
+server.use('/api/users', userRouter);
 
 //custom middleware
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -12,15 +12,37 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  userDb.get()
+  .then(people =>{
+    console.log('success on GET api/users request');
+    res.status(200).json(people)
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getUserPosts(req.user.id)
+  .then(articles =>{
+    if(articles.length === 0){
+      res.status(404).json({Eror:"This user doesn't seem to have any posts made."})
+    }
+    else{
+      res.status(200).json(articles);
+    }
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
@@ -36,9 +58,11 @@ router.put('/:id', validateUserId, (req, res) => {
 function validateUserId(req,res,next){
   userDb.getById(req.params.id)
   .then(person =>{
-    if(person.length === 0){
+    if(person === undefined){
       res.status(400).json({ message: "invalid user id" });  
     }else{
+      console.log(person);
+      req.user = person;
       next();
     }
   })
@@ -50,6 +74,8 @@ function validateUserId(req,res,next){
 
 function validateUser(req, res, next) {
   // do your magic!
+  if(req.body.name === "" || req.body.name === undefined)
+
 }
 
 function validatePost(req, res, next) {
